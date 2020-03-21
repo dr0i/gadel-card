73afd8b7c731c356f8e0769f7e1089eda5774497
Pascal Christoph <christoph@hbz-nrw.de>
2020-03-21 16:18:21 +0100

    Add helper function; compute and render fight

--- js/deck.js
+++ js/deck.js
@@ -1,35 +1,67 @@
-import * as data from '/gadel-card/data/characterCards.js';
-import * as helper from '/gadel-card/js/helper.js';
-//characterCards: left
-console.log("DeckLeftCharacter");
-var DeckLeftCharacter = helper.rnd(0,2);
+import * as data from "/gadel-card/data/characterCards.js";
+import * as helper from "/gadel-card/js/helper.js";
+//characterCards left
+var DeckLeftCharacter = helper.rnd(0, 2);
+var leftCharacter = data.default.characters[DeckLeftCharacter];
 var DeckLeft = document.getElementById("left");
-DeckLeft.innerHTML += data.default.characters[DeckLeftCharacter].name 
-+'<img src="' + data.default.characters[DeckLeftCharacter].image +'" style="max-width:300px"/>'
-+ '"<p style="margin-top: -1em;color: white; text-align: left">' 
-+ "Phi:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + data.default.characters[DeckLeftCharacter].properties[0]  + "</br>"
-+ "Stärke:&nbsp;&nbsp;&nbsp;" + data.default.characters[DeckLeftCharacter].properties[1]  + "</br>"
-+ "Einfluss:&nbsp;" + data.default.characters[DeckLeftCharacter].properties[2]
-+ '</p>';
-//characterCards: right
+//characterCard: right
 var DeckRight = document.getElementById("right");
-console.log("DeckRightCharacter");
-var DeckRightCharacter = helper.rnd(0,2,DeckLeftCharacter);
-console.log("DeckRightCharacter:" + DeckRightCharacter);
+var rightCharacter = data.default.characters[helper.rnd(0, 2, DeckLeftCharacter)];
 
-DeckRight.innerHTML += data.default.characters[DeckRightCharacter].name 
-+'<img src="' + data.default.characters[DeckRightCharacter].image +'" style="max-width:300px"/>'
-+ '"<p style="margin-top: -1em; color: white; text-align: left">' 
-+ "Phi:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + data.default.characters[DeckRightCharacter].properties[0] + "</br>"
-+ "Stärke:&nbsp;&nbsp;&nbsp;" + data.default.characters[DeckRightCharacter].properties[1] + "</br>"
-+ "Einfluss:&nbsp;" + data.default.characters[DeckRightCharacter].properties[2] + "</br>"
-+ '</p>';
+DeckLeft.innerHTML +=
+  leftCharacter.name +
+  '<img src="' +
+  leftCharacter.image +
+  '" style="outline: 2px solid yellow; max-width:300px"/>' +
+  '"<p style="outline: 2px solid yellow;margin-top: -1em;color: white; text-align: left">' +
+  "Phi:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+  leftCharacter.properties[0] +
+  "</br>" +
+  "Stärke:&nbsp;&nbsp;&nbsp;" +
+  leftCharacter.properties[1] +
+  "</br>" +
+  "Einfluss:&nbsp;" +
+  leftCharacter.properties[2] +
+  "</p>";
+
+DeckRight.innerHTML +=
+  rightCharacter.name +
+  '<img src="' +
+  rightCharacter.image +
+  '" style="outline: 2px solid blue; max-width:300px"/>' +
+  '"<p style="outline: 2px solid blue; margin-top: -1em; color: white; text-align: left">' +
+  "Phi:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" +
+  rightCharacter.properties[0] +
+  "</br>" +
+  "Stärke:&nbsp;&nbsp;&nbsp;" +
+  rightCharacter.properties[1] +
+  "</br>" +
+  "Einfluss:&nbsp;" +
+  rightCharacter.properties[2] +
+  "</br>" +
+  "</p>";
 
 var Deck = document.getElementById("deck");
 
 // who has the longest?
-Deck.innerHTML += "1. Runde: </br>" ;
+Deck.innerHTML += "1. Runde: </br>";
 
-
-Deck.innerHTML + data.default.characters[0].properties[0] +" vs. " + data.default.characters[1].properties[0]; 
+var propRnd = helper.rnd(0, 2);
+console.log("propRnd:" +propRnd)
+var fightOutcome = helper.fightOutcome(leftCharacter.properties[propRnd], rightCharacter.properties[propRnd]);
+Deck.innerHTML +=
+  '<span style="color: yellow" >'+ leftCharacter.name + 
+  " </span> setzt den Wert " +
+  data.default.fields[propRnd] +
+  " (<span style=\"color: yellow\"> " +
+  leftCharacter.properties[propRnd] +
+  " </span> ) ein vs. <span style=\"color: #9ba9e9\" >" +
+  rightCharacter.name +
+  " </span>  mit dem Wert  " +
+  data.default.fields[propRnd] +
+  " ( <span style=\"color: #9ba9e9\">" +
+  rightCharacter.properties[propRnd] +
+  " </span>) </br>" +
+  '<span style="color: yellow" >'+ leftCharacter.name + 
+  " </span> "+ fightOutcome +" !";
 
--- js/helper.js
+++ js/helper.js
@@ -1,10 +1,14 @@
-export function rnd(min,max,not){
-    var res=Math.floor(Math.random()*(max-min+1)+min );
-    console.log("res: "+ res +", not" + not)
-    while ( res === not) {
-        console.log("res: "+ res +" == not:" + not)
-        res=Math.floor(Math.random()*(max-min+1)+min );
-    }
-    return res;
-   
+export function rnd(min, max, not) {
+  var res = Math.floor(Math.random() * (max - min + 1) + min);
+  while (res === not) {
+    res = Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  return res;
+}
+export function fightOutcome(leftProp, rightProp) {
+    console.log("leftProp:" +leftProp, "rightProp:" + rightProp)
+
+    if (leftProp >= rightProp) {
+      return "gewinnt";}
+  else return "verliert";
 }
