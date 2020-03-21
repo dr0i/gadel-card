9623cb376729c86d174f97afa76b33e6f8159daf
Pascal Christoph <christoph@hbz-nrw.de>
2020-03-21 18:21:24 +0100

    Fix html; format

--- js/deck.js
+++ js/deck.js
@@ -1,13 +1,15 @@
 import * as data from "../data/characterCards.js";
 import * as helper from "./helper.js";
 //characterCards left
-var DeckLeftCharacter = helper.rnd(0, data.default.characters.length-1);
+var DeckLeftCharacter = helper.rnd(0, data.default.characters.length - 1);
 var leftCharacter = data.default.characters[DeckLeftCharacter];
 var DeckLeft = document.getElementById("left");
 //characterCard: right
 var DeckRight = document.getElementById("right");
 var rightCharacter =
-  data.default.characters[helper.rnd(0, data.default.characters.length-1, DeckLeftCharacter)];
+  data.default.characters[
+    helper.rnd(0, data.default.characters.length - 1, DeckLeftCharacter)
+  ];
 
 DeckLeft.innerHTML +=
   leftCharacter.name +
@@ -47,7 +49,7 @@ var Deck = document.getElementById("deck");
 // who has the longest?
 Deck.innerHTML += "1. Runde: </br>";
 
-var propRnd = helper.rnd(0, data.default.fields.length-1);
+var propRnd = helper.rnd(0, data.default.fields.length - 1);
 var fightOutcome = helper.fightOutcome(
   leftCharacter.properties[propRnd],
   rightCharacter.properties[propRnd]
@@ -70,4 +72,5 @@ Deck.innerHTML +=
   leftCharacter.name +
   " </span> " +
   fightOutcome +
-  " ! </br> </br><button type=\"button\" onclick=\"window.location.reload();\">Neue Runde</button>";
+  ' ! </br> </br><button type="button" onclick="window.location.reload();">Neue Runde</button>' +
+  "</br></br><a href=\"https://gadel.org/galerie2.html\">Bilder-Quelle und Lizenz</a></body>";
